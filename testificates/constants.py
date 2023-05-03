5c369c5c96873e9a68fb9283ec328a5f9759f593
JobAbramov <darthdictionary@yandex.ru>
2023-05-03 16:39:12 +0300

    requirements and env added

--- testificates/constants.py
+++
@@ -1,4 +0,0 @@
-TOKEN = 'pkreExsxYRea-t0eSwUQKQVZVIJwHf7TqQw_8KDoyJrfVU30mSu2MHdkFgWCZMPBy_LAebmRGdfagaXaiwd3aA=='
-ORG = "home"
-URL = "http://localhost:8086"
-BUCKET="main"
--- testificates/influx_test.py
+++ testificates/influx_test.py
@@ -1,10 +1,15 @@
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-import constants as const
 import psutil as pc
+import dotenv as dnv
 
-write_client = influxdb_client.InfluxDBClient(url = const.URL, token = const.TOKEN, org = const.ORG)
+URL = dnv.get_key('.env', 'URL')
+TOKEN = dnv.get_key('.env', 'TOKEN')
+ORG = dnv.get_key('.env', 'ORG')
+BUCKET = dnv.get_key('.env', 'BUCKET')
+
+write_client = influxdb_client.InfluxDBClient(url = URL, token = TOKEN, org = ORG)
 
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
    
@@ -14,12 +19,12 @@ for value in range(100):
     .tag("comp", "local")
     .field("cpu_load_percent", pc.cpu_percent())
   )
-  write_api.write(bucket = const.BUCKET, org = const.ORG, record = point)
+  write_api.write(bucket = BUCKET, org = ORG, record = point)
   time.sleep(0.5)
 
 read_api = write_client.query_api()
 
 query = '''from(bucket: "main")
-  |> range(start: -45m)
+  |> range(start: -1d)
   |> filter(fn: (r) => r["_measurement"] == "cpu_load")'''
-print(read_api.query(org = const.ORG, query = query).to_json())
+print(read_api.query(org = ORG, query = query).to_json())
